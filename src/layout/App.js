a0f465141e9e4ebe9fa24745f2e34d929162d481
Thousif jowhar <thousif20@gmail.com>
2017-09-25 11:13:13 +0530

    adding header

--- src/layout/App.js
+++ src/layout/App.js
@@ -4,7 +4,7 @@ import './App.css'
 import { fetchOpportunities } from "../actions/Actions"
 import { connect } from 'react-redux'
 import { Layout, Menu, Icon, notification } from 'antd'
-const { Sider, Content } = Layout;
+const { Sider, Content, Header } = Layout;
 
 @connect((store) => {
   console.log(store);
@@ -33,7 +33,19 @@ class App extends Component {
     return (
         <div >
       	<Layout style={{minHeight : '100vh'}}>
-        
+          <Header>
+            <div className="logo" />
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={['2']}
+              style={{ lineHeight: '64px' }}
+            >
+              <Menu.Item key="1">nav 1</Menu.Item>
+              <Menu.Item key="2">nav 2</Menu.Item>
+              <Menu.Item key="3">nav 3</Menu.Item>
+            </Menu>
+          </Header>
         <Layout>
         </Layout>
       </Layout>
